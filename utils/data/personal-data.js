42d0e49dca582941c0fab663d339d85be5c728cc
springshine0925 <wilsonchan020@gmail.com>
2024-05-18 01:30:12 +0800

    removeunneeded code

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -4,7 +4,7 @@ export const personalData = {
   designation: "Blockchain Web Engineer",
   description: "My name is Wilson Chan. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'wilsonchan020@gmail.com',
-  phone: '+8526978390',
+  phone: '',
   address: 'Hong Kong',
   github: 'https://github.com/springshine0925',
   facebook: 'https://www.facebook.com/abusaid.riyazasgaeghaggawegawe/',
