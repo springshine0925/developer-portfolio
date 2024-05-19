bac98f832f29d4e70238e65901324ecd586523cf
springshine0925 <wilsonchan020@gmail.com>
2024-05-19 11:16:27 +0800

    fix experience.js a little

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -18,6 +18,6 @@ export const experiences = [
     title: "JUNIOR DEVELOPER  ",
     company: "Pilotcore | Canada",
     duration: "(06/2017 - 11/2019)", 
-    role: "*Organized overall strategies, business operates, and what drives success in the  siness. <br/>    *Collaborated with 2 engineering teams to integrate external APIs   into web pages and applications.     Evaluated and improved existing data system under mentor   supervision.    Worked on Web3 integration on Ethereum, Solana, and Casper     ificcing"
+    role: "*Organized overall strategies, business operates, and what drives success in the  siness. <br/>    *Collaborated with 2 engineering teams to integrate external APIs   into web pages and applications.     Evaluated and improved existing data system under mentor   supervision.    Worked on Web3 integration on Ethereum, Solana, and Casper"
   }
 ]
